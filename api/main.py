22fecb4ebe40dfa2e98b7b24405ad7d067337cbe
Hoogashaga <shchen6536@gmail.com>
2023-05-07 00:44:26 -0700

    Linter

--- api/main.py
+++ api/main.py
@@ -8,12 +8,12 @@ from flask_cors import CORS
 load_dotenv(dotenv_path="./.env.local")
 # print(os.environ.get("UNSPLASH_KEY", ""))
 
-URL="https://api.unsplash.com/photos/random"
-UNSPLASH_KEY=os.environ.get("UNSPLASH_KEY", "")
-DEGUB=bool(os.environ.get("DEBUG", True))
+URL = "https://api.unsplash.com/photos/random"
+UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")
+DEGUB = bool(os.environ.get("DEBUG", True))
 
 if not UNSPLASH_KEY:
-    raise EnvironmentError("UNSPLASH_KEY is not set, please create a .env.local file and set the UNSPLASH_KEY environment variable")
+    raise EnvironmentError("UNSPLASH_KEY is not set, please createa .env.local file and set the UNSPLASH_KEY environment variable")
 
 # new isinstance of Flask
 app = Flask(__name__)
@@ -22,7 +22,6 @@ CORS(app)
 # degun mode is on
 app.config["DEBUG"] = DEGUB
 
-# app.route("/")(hello)
 @app.route("/new-image")
 def new_image():
     word = request.args.get("query")
@@ -34,7 +33,6 @@ def new_image():
     res = requests.get(url=URL, headers=headers, params=params)
     data = res.json()
     return data
-  
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050)
-  
